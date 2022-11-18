363048d792c7bf450d9a472bf354ca373b6cd283
tiredtiger000 <114841392+tiredtiger000@users.noreply.github.com>
2022-11-17 20:24:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import foodData from "./assets/kfood-data.json";
+import { Layout } from 'antd';
+import Header from './components/Header';
+import Main from './components/Main';
+import Basket from './components/Basket';
+import {Row, Col, Divider} from 'antd';
+const {Sider} = Layout;
 
+document.body.style.backgroundColor = "antiquewhite";
 function App() {
+  const [foodList, setList] = useState(foodData)
+  function updateFoodList(updatedList){
+    setList([...updatedList])
+  }
+  const [cartItems, setCartItems] = useState([])
+
+  function updateCart(item) {
+    setCartItems([...cartItems, item])
+  }
+  function grandTotal(items) {
+    const myPrices = items.map(item => item.price)
+    const sum = myPrices.reduce((price, item) => price + item, 0)
+    return sum
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Row gutter={[1,10]}>
+        <Col xs={25} md={16}>
+          <Row gutter={[8,15]}>
+            <Divider></Divider>
+            {foodList.map((item) => (<Main foodItem = {item} addToCart={updateCart}></Main>))}
+          </Row>
+        </Col>
+        <Col xs={25} md={5} offset={2}>
+          <Basket foodData={foodData} updateFoodList={updateFoodList}> </Basket>
+          <h3>My Cart</h3>
+          {cartItems.map((item) => (<p>{item.name} ${item.price}</p>))}
+          <h4>Grand Total: ${grandTotal(cartItems)}</h4>
+        </Col>
+      </Row>
     </div>
   );
 }
-
 export default App;
